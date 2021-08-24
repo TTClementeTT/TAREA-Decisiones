1ded327a4d64f30cd1a6915951d7719fba54c2ec
TTClementeTT <88836941+TTClementeTT@users.noreply.github.com>
2021-08-24 14:10:18 -0500

    Update exercise.py

--- assignments/08PuntoCircunferencia/src/exercise.py
+++ assignments/08PuntoCircunferencia/src/exercise.py
@@ -2,7 +2,23 @@ import math
 
 def main():
     # Escribe tu código abajo de esta línea
+    from math import sqrt
+radio= float(input("Dame el radio="))
+coordenadax1= float(input("X1 Coordenada x del centro de la circunferencia="))
+coordenaday1= float(input("Y1 Coordenada y del centro de la circunferencia="))
+coordenadax2punto= float(input("X2 Coordenada x del punto="))
+coordenaday2punto= float(input("Y2 Coordenada y del punto="))
+
+distancia= sqrt((coordenadax2punto-coordenadax1)**2 + (coordenaday2punto-coordenaday1)**2)
+
+if distancia<radio:
+    print("DENTRO")
+elif distancia==radio:
+    print("SOBRE")
+else:
+    print("FUERA")
     
+
 
 if __name__ == '__main__':
     main()
