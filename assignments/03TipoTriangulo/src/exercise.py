ed1d6aae3e3ba2752d084141d82d303fe18a5f09
TTClementeTT <88836941+TTClementeTT@users.noreply.github.com>
2021-08-24 14:09:00 -0500

    Update exercise.py

--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -1,6 +1,28 @@
 def main():
     #escribe tu código abajo de esta línea
-  
+  print("todos los numeros son mayores a 0")
+
+valora=int(input("ingresa el valor a="))
+valorb=int(input("ingresa el valor b="))
+valorc=int(input("ingresa el valor c="))
+
+
+if(valora+valorb>valorc and valora+valorc>valorb and valorb+valorc>valora):
+    print("ES TRIANGULO")
+else:
+    print("NO ES TRIANGULO")
+
+if(valora==valorb and valora==valorc):
+    print("EQUILATERO")
+
+elif(valora!=valorb and valora!=valorc and valorb!=valorc):
+    print("ESCALENO")
+
+else:
+    print("ISOSCELES")
+     
+
+
 
 
 if __name__=='__main__':
