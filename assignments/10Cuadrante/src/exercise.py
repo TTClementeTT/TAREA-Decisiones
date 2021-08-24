b9a80f9e56d9fec1fc5bf1e653e957e8bc9270fd
TTClementeTT <88836941+TTClementeTT@users.noreply.github.com>
2021-08-24 14:11:39 -0500

    Update exercise.py

--- assignments/10Cuadrante/src/exercise.py
+++ assignments/10Cuadrante/src/exercise.py
@@ -1,6 +1,33 @@
 def main():
     # Escribe tu código abajo de esta línea
-   
+   a=int(input("ingresa el valor en grados="))
+if(a<90 and a>0):
+    print("se encuentra en el cuadrante 1")
+elif(a==90):
+    print("eje")
+elif(a>90 and a<180):
+    print("se encuentra en el cuadrante 2")   
+if(a==180):
+    print("eje")
+elif(a>180 and a<270):
+    print("se encuentra en el cuadrante 3")
+if(a==270):
+    print("eje")     
+elif(a>270 and a<360):
+    print("se encuentra en el cuadrante 4")
+
+if(a==360):
+    print("eje")
+    
+elif(a==0):
+    print("excede")
+elif(a<0):
+    print("excede")
+elif(a>360):
+    print("excede") 
+    
+
+
 
 if __name__ == '__main__':
     main()
