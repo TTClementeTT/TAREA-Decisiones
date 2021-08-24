f58b44dce999439463e5482c0e54164b5aaac812
TTClementeTT <88836941+TTClementeTT@users.noreply.github.com>
2021-08-24 14:08:16 -0500

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -2,7 +2,23 @@
 def main():
     #escribe tu código abajo de esta línea
   
+    def main():
+    #escribe tu código abajo de esta línea
+  
+    numero=int(input("Dame el número: "))
     
+    if numero >= 1:
+        print("Es positivo ")
+    elif numero == 0:
+        print("Es cero ")
+    else:
+        print("Es negativo ")
+        
+
+if __name__ == '__main__':
+    main()
+
+
 
 if __name__ == '__main__':
     main()
